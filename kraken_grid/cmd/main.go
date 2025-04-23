4cadbc55855370872c00d90386bf9fb3409d7ea0
dafsic <dafsic@gmail.com>
2025-04-23 16:37:31 +0800

    Fix: GRPC server does not run

--- kraken_grid/cmd/main.go
+++ kraken_grid/cmd/main.go
@@ -5,8 +5,10 @@ import (
 
 	"github.com/dafsic/toad/app"
 	"github.com/dafsic/toad/kraken"
+	"github.com/dafsic/toad/kraken_grid/api"
 	"github.com/dafsic/toad/kraken_grid/bot"
 	"github.com/dafsic/toad/kraken_grid/dao"
+	"github.com/dafsic/toad/kraken_grid/server"
 	"github.com/dafsic/toad/log"
 	"github.com/dafsic/toad/postgres"
 )
@@ -19,6 +21,8 @@ func main() {
 		&dao.Module{},
 		&bot.Module{},
 		&postgres.Module{},
+		&api.Module{},
+		&server.Module{},
 	)
 	if err := app.Run(); err != nil {
 		fmt.Println(err)
